20a78ef1d920ab86f9b8355c9e0c744a44b40848
Alexey_Berezin <alexey_berezin@epam.com>
2018-03-17 14:42:28 +0300

    prod: update room route

--- app/routes/room_routes.js
+++ app/routes/room_routes.js
@@ -1,12 +1,32 @@
 module.exports = function(app, db) {
+  // Add room
   app.post('/rooms', (req, res) => {
-    const node = { text: req.body.body, title: req.body.title };
+    const note = { 
+      text: req.body.body, 
+      title: req.body.title, 
+      roomId: req.body.roomId
+    };
+
     db.collection('rooms').insert(note, (err, result) => {
       if (err) {
-        res.send({ error: 'An error has occurred' });
+        res.send({ error: 'An error has occurred while adding rooms' });
       } else {
         res.send(result.ops[0]);
       }
     });
+  });
+  // Get a room
+  app.get('/rooms/:id', (req, res) => {
+    const roomId = req.params.id
+    
+    db.collection('rooms')
+      .find({ roomId })
+      .toArray((err, rooms) => {
+        if (err) {
+          res.send({ error: 'An error has occured while getting rooms' });
+        } else {
+          res.send(rooms);
+        }
+      })
   });
 };
